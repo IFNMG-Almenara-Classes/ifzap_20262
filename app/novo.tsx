39de2d33303cdcc8c3c7d16e1d0c29fce61fada4
Leonan Oliveira <leonan.teixeira@gmail.com>
2026-03-18 21:29:20 -0300

    inputs]

--- app/novo.tsx
+++ app/novo.tsx
@@ -3,20 +3,28 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function NovoPage() {
   const [nome, setNome] = useState("");
+  const [telefone, setTelefone] = useState("");
+
   return (
     <View style={{ flex: 1 }}>
       <Text>Nome: {nome}</Text>
       <TextInput
-        style={styles.inputNome}
+        style={styles.input}
         value={nome}
         onChangeText={(novoNome) => setNome(novoNome)}
+      />
+      <Text>Telefone: {telefone}</Text>
+      <TextInput
+        style={styles.input}
+        value={telefone}
+        onChangeText={(novoTelefone) => setTelefone(novoTelefone)}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  inputNome: {
+  input: {
     borderColor: "#7454cc",
     borderWidth: 1,
     margin: 5,
